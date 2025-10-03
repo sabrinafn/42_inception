0e61c402b5d632f4250e3f2ea83bed6ea6271679
sabrina <sabrinaferreiran@gmail.com>
2025-10-03 16:42:34 -0300

    feat: add .env.example and entrypoint script for MariaDB setup; update Dockerfile

---
+++ srcs/requirements/MariaDB/config/entrypoint.sh
@@ -0,0 +1,14 @@
+# Shell + SQL Initialization Script Checklist
+
+# Script header
+#!/bin/sh
+
+# Wait for MariaDB service to run
+
+# Set the root password
+
+# Create the initial database
+
+# Apply privileges
+
+# Keep MariaDB running, ensure the MariaDB process stays in the foreground so the container does not exit.
