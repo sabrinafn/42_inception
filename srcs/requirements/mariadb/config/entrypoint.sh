f1448b994186132441727c27fb3b31fcfe367772
sabrina <sabrinaferreiran@gmail.com>
2025-10-14 21:16:45 -0300

    feat: update Docker setup for MariaDB, Nginx, and WordPress; add configuration files and entrypoint scripts

--- srcs/requirements/mariadb/config/entrypoint.sh
+++ srcs/requirements/mariadb/config/entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-set -e
 
-# init directory in which it has system tables
-[ ! -d "/var/lib/mysql/mysql" ] && mysql_install_db --user=mysql --datadir=/var/lib/mysql
-
-# Configure root user
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'pass123';"
-
-# Create database
-mysql -e "CREATE DATABASE IF NOT EXISTS db;"
-
-# Grant privileges to root user
-mysql -e "GRANT ALL PRIVILEGES ON db.* TO 'root'@'localhost'; FLUSH PRIVILEGES;"
-
-# Execute mariadb
-exec runuser -u mysql -- mariadbd --console
+mysql_install_db
+mysqld
