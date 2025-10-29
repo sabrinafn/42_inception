a700c9256c51308787fe9c007d7003817aa82d34
sabrina <sabrinaferreiran@gmail.com>
2025-10-28 22:17:20 -0300

    fix: update MariaDB entrypoint script and docker-compose for root password handling; streamline Nginx configuration

--- srcs/requirements/mariadb/config/entrypoint.sh
+++ srcs/requirements/mariadb/config/entrypoint.sh
@@ -1,8 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-mysql_install_db
-mysqld
+mkdir -p /var/lib/mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
 
-while ! mysqladmin ping -h localhost --silent; do
-    sleep 1
-done
+cat <<EOF > /etc/mysql/init.sql
+CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
+CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+chown mysql:mysql /etc/mysql/init.sql
+
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+fi
+
+# init mariadb
+exec mysqld_safe --datadir=/var/lib/mysql
